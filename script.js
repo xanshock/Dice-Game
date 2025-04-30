462c36a1a3e6500760a3aa79c88ed257e85f1251
xanshock <xanshock.design@gmail.com>
2025-04-30 22:25:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var randomNum = Math.floor(Math.random()*6) + 1;
+var randomDice = "dice" +randomNum+".png";
+var randomImage = "./images/" + randomDice;
+
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute("src",randomImage);
+
+var randomNum1 = Math.floor(Math.random()*6)+1;
+var randomDice1 = "dice" + randomNum1 + ".png";
+var randomImage1 = "./images/" + randomDice1;
+
+var img2 = document.querySelectorAll("img")[1];
+img2.setAttribute("src", randomImage1);
+
+if (randomNum > randomNum1){
+    document.querySelector("h1").innerHTML = "Player 1 win..."
+}else{
+    document.querySelector("h1").innerHTML = "Player 2 win...";
+}
