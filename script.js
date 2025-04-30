c346046e18c0b7d990a8bcf0a169580164ab05f4
xanshock <xanshock.design@gmail.com>
2025-04-30 22:31:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-var randomNum = Math.floor(Math.random()*6) + 1;
-var randomDice = "dice" +randomNum+".png";
+var randomNum = Math.floor(Math.random() * 6) + 1;
+var randomDice = "dice" + randomNum + ".png";
 var randomImage = "./images/" + randomDice;
 
 var img1 = document.querySelectorAll("img")[0];
-img1.setAttribute("src",randomImage);
+img1.setAttribute("src", randomImage);
 
-var randomNum1 = Math.floor(Math.random()*6)+1;
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
 var randomDice1 = "dice" + randomNum1 + ".png";
 var randomImage1 = "./images/" + randomDice1;
 
 var img2 = document.querySelectorAll("img")[1];
 img2.setAttribute("src", randomImage1);
 
-if (randomNum > randomNum1){
-    document.querySelector("h1").innerHTML = "Player 1 win..."
-}else{
-    document.querySelector("h1").innerHTML = "Player 2 win...";
+if (randomNum > randomNum1) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNum < randomNum1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
 }
